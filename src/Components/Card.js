4166c3c3db00177547240b4a8f5dab777331ab5c
Russell Hart <russellbot@gmail.com>
2020-06-21 21:19:09 -0400

    colors

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
     return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-yellow dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${id}?size=250x250`} />
             <div>
                 <h2>{name}</h2>
